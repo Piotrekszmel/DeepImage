a1aca115fb3d16a142c3bbfb8f59a276c38fd67e
Piotrekszmel <piotr.szmelczynski@gmail.com>
2022-01-29 18:15:12 +0100

    make photo_demo works with webcam

--- core/style_transfer/photo_demo.py
+++ core/style_transfer/photo_demo.py
@@ -1,12 +1,9 @@
 import cv2
-import numpy as np
 import torch
-from torch.autograd import Variable
 
 from net import Net
 from option import Options
-from utils import StyleLoader
-
+from utils import StyleLoader, predict_image
 
 def run_demo(args, mirror=False):
     style_model = Net(ngf=args.ngf)
@@ -24,73 +21,47 @@ def run_demo(args, mirror=False):
         style_loader = StyleLoader(args.style_folder, args.style_size, False)
 
     # Define the codec and create VideoWriter object
-    height =  args.demo_size
-    width = int(4.0 / 3 * args.demo_size)
-    swidth = int(width / 4)
-    sheight = int(height / 4)
-    # if args.record:
-    #     fourcc = cv2.VideoWriter_fourcc('F','M','P','4')
-    #     out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (2*width, height))
-    cam = cv2.VideoCapture(0)
-    cv2.namedWindow("photo")
-    cam.set(3, width)
-    cam.set(4, height)
-    key = 0
-    img_counter = 0
-    while True:
-        ret, img = cam.read()
-        if not ret:
-            print("Failed to grab frame")
-            break
-        cv2.imshow("photo", img)
-        
-        k = cv2.waitKey(1)
-        if k % 256 == 27:
-            # ESC pressed
-            print("Escape hit, closing...")
-            break
-        elif k % 256 == 32:
-            # SPACE pressed
-            # img_name = "opencv_img_{}.png".format(img_counter)
-            # cv2.imwrite(img_name, img)
-            # print("{} written!".format(img_name))
-            # img_counter += 1
-            if mirror: 
-                img = cv2.flip(img, 1)
-            cimg = img.copy()
-            img = np.array(img).transpose(2, 0, 1)
-
-            style = style_loader.get(8)
-            style_model.setTarget(style)
-
-            img = torch.from_numpy(img).unsqueeze(0).float()
-            if args.cuda:
-                img = img.cuda()
-
-            img = style_model(img)
-
-            if args.cuda:
-                simg = style.cpu()[0].numpy()
-                img = img.cpu().clamp(0, 255)[0].numpy()
-            else:
-                simg = style.numpy()
-                img = img.clamp(0, 255).detach().numpy()
-            simg = np.squeeze(simg)
-            img = img.squeeze().transpose(1, 2, 0).astype('uint8')
-            simg = simg.transpose(1, 2, 0).astype('uint8')
-
-            # display
-            simg = cv2.resize(simg,(swidth, sheight), interpolation = cv2.INTER_CUBIC)
-            cimg[0:sheight, 0:swidth, :] = simg
-            #img = np.concatenate((cimg, img),axis=1)
-            img_name = "opencv_img_{}.png".format(img_counter)
-            cv2.imwrite(img_name, img)
-            cv2.imshow('MSG Demo', img)
-            key = cv2.waitKey(1)
-            if key % 256 == 27:
+    if args.content_image:
+        content_image = cv2.imread(args.content_image)
+        pred_img = predict_image(content_image,
+                            style_loader,
+                            style_model,
+                            mirror,
+                            args.style_idx,
+                            args.cuda)  
+    else:
+        height =  args.demo_size
+        width = int(4.0 / 3 * args.demo_size)
+        cam = cv2.VideoCapture(0)
+        cv2.namedWindow("photo")
+        cam.set(3, width)
+        cam.set(4, height)
+        key = 0
+        while True:
+            ret, img = cam.read()
+            if not ret:
+                print("Failed to grab frame")
                 break
-
-    cam.release()
+            cv2.imshow("photo", img)
+            
+            k = cv2.waitKey(1)
+            if k % 256 == 27:
+                # ESC pressed
+                print("Escape hit, closing...")
+                break
+            elif k % 256 == 32:
+                # SPACE pressed
+                pred_img = predict_image(img,
+                                    style_loader,
+                                    style_model,
+                                    mirror,
+                                    args.style_idx,
+                                    args.cuda)
+                cam.release()
+                break
+    img_name = "opencv_img_{}.png".format(args.style_idx)
+    cv2.imwrite(img_name, pred_img)
+    cv2.imshow('MSG Demo', pred_img)
     cv2.destroyAllWindows()
 
 def main():

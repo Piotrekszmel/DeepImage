88e3a224cf2aad6786302902da03b346e1c80064
Piotrekszmel <piotr.szmelczynski@gmail.com>
2022-02-02 22:02:37 +0100

    modify main.py

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
+from crypt import methods
 import os
 from flask import Flask, request, redirect, url_for, send_from_directory, render_template
 
 
 UPLOAD_FOLDER = './'
-ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'png'])
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ['jpg', 'jpeg', 'png']
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
+@app.route("/", methods=["GET", "POST"])
+def index():
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
@@ -31,13 +31,13 @@ def upload_file():
             filename_out = 'out.jpg'
             # ffwd_to_img(filename, filename_out, request.form['checkpoint'])
             return redirect(url_for('uploaded_file',
-                                    filename=filename_out))
-    return render_template('./index.html')
+                                    filename=filename))
+    return render_template("index.html")
 
-@app.route('/uploads<filename>')
+@app.route('/uploads<filename>', methods=["GET"])
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)
+                               filename, as_attachment=True)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1")

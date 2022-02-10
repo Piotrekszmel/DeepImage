e94e737318e67a8c3f1c68a074188361bbec69eb
bartekkz <bartek.szmelczynski@gmail.com>
2022-02-10 19:31:21 +0100

    change opencv image capturing to pygame

--- core/style_transfer/photo_demo.py
+++ core/style_transfer/photo_demo.py
@@ -1,6 +1,9 @@
 from configparser import Interpolation
 import cv2
 import torch
+import os
+import pygame
+import pygame.camera
 
 from .net import Net
 from .utils import StyleLoader, predict_image
@@ -16,45 +19,27 @@ def predict(args):
     style_model.eval()
     style_loader = StyleLoader(args["style_folder"], args["style_size"])
 
-    if "content_image" in args:
-        content_image = cv2.imread(args["content_image"])
-        pred_img = predict_image(content_image,
-                                 style_loader,
-                                 style_model,
-                                 args["mirror"],
-                                 args["style_idx"])  
-    else:
-        height =  args["demo_size"]
-        width = int(4.0 / 3 * args["demo_size"])
-        cam = cv2.VideoCapture(0)
-        #cv2.namedWindow("photo")
-        cam.set(3, width)
-        cam.set(4, height)
-        key = 0
-        while True:
-            ret, img = cam.read()
-            if not ret:
-                print("Failed to grab frame")
-                break
-            cv2.imshow("photo", img)
-            
-            k = cv2.waitKey(1)
-            if k % 256 == 27:
-                # ESC pressed
-                print("Escape hit, closing...")
-                break
-            elif k % 256 == 32:
-                # SPACE pressed
-                pred_img = predict_image(img,
-                                         style_loader,
-                                         style_model,
-                                         args["mirror"],
-                                         args["style_idx"])
-                cam.release()
-                break
-    
+    content_image = cv2.imread(args["content_image"])
+    pred_img = predict_image(content_image,
+                                style_loader,
+                                style_model,
+                                args["mirror"],
+                                args["style_idx"])  
     if ("resize" in args):
         if ("new_height" in args and "new_width" in args):
             pred_img = cv2.resize(pred_img, (args["new_width"], args["new_height"]), interpolation = cv2.INTER_AREA)
     cv2.imwrite(args["output_image"], pred_img)
     cv2.destroyAllWindows()
+
+
+def make_photo(height: int,
+               save_path: str) -> None:
+
+    pygame.camera.init()
+    pygame.camera.list_cameras() #Camera detected or not
+    cam = pygame.camera.Camera("/dev/video0",(800,600))
+    cam.start()
+    img = cam.get_image()
+    pygame.image.save(img, save_path)
+    cam.stop()
+    

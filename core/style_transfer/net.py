ae57f6b373584ac9daaf62e44130526aa01f28f6
Piotrekszmel <piotr.szmelczynski@gmail.com>
2022-02-11 20:56:48 +0100

    add information about types for inputs and outputs for net.py and photo_demo.py

--- core/style_transfer/net.py
+++ core/style_transfer/net.py
@@ -1,83 +1,14 @@
 import numpy as np
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-
-def var(x, dim=0):
-    '''
-    Calculates variance.
-    '''
-    x_zero_meaned = x - x.mean(dim).expand_as(x)
-    return x_zero_meaned.pow(2).mean(dim)
-
-
-class MultConst(nn.Module):
-    def forward(self, input):
-        return 255 * input
 
 
 class GramMatrix(nn.Module):
-    def forward(self, y):
+    def forward(self, y: torch.Tensor) -> torch.Tensor:
         (b, ch, h, w) = y.size()
         features = y.view(b, ch, w * h)
         features_t = features.transpose(1, 2)
         return features.bmm(features_t) / (ch * h * w)
-    
-
-class Basicblock(nn.Module):
-    def __init__(self, 
-                 inplanes,
-                 planes,
-                 stride=1,
-                 downsample=None,
-                 norm_layer=nn.BatchNorm2d):
-        super(Basicblock, self).__init__()
-        self.downsample = downsample
-        if self.downsample is not None:
-            self.residual_layer = nn.Conv2d(inplanes, 
-                                            planes,
-                                            kernel_size=1,
-                                            stride=stride)
-        conv_block = [norm_layer(inplanes),
-                      nn.ReLU(inplace=True),
-                      ConvLayer(inplanes, planes, kernel_size=3, stride=stride),
-                      norm_layer(planes),
-                      nn.ReLU(inplace=True),
-                      ConvLayer(planes, planes, kernel_size=3, stride=1),
-                      norm_layer(planes)]
-        self.conv_block = nn.Sequential(*conv_block)
-    
-    def forward(self, input):
-        if self.downsample is not None:
-            residual = self.residual_layer(input)
-        else:
-            residual = input
-        return residual + self.conv_block(input)
-            
-
-class UpBasicblock(nn.Module):
-    """ Up-sample residual block (from MSG-Net paper)
-    Enables passing identity all the way through the generator
-    ref https://arxiv.org/abs/1703.06953
-    """
-    def __init__(self, 
-                 inplanes,
-                 planes,
-                 stride=2,
-                 norm_layer=nn.BatchNorm2d):
-        super(UpBasicblock, self).__init__()
-        self.residual_layer = UpsampleConvLayer(inplanes, planes,
-                                                      kernel_size=1, stride=1, upsample=stride)
-        conv_block = [norm_layer(inplanes),
-                      nn.ReLU(inplace=True),
-                      UpsampleConvLayer(inplanes, planes, kernel_size=3, stride=1, upsample=stride),
-                      norm_layer(planes),
-                      nn.ReLU(inplace=True),
-                      ConvLayer(planes, planes, kernel_size=3, stride=1)]
-        self.conv_block = nn.Sequential(*conv_block)
-    
-    def forward(self, input):
-        return self.residual_layer(input) + self.conv_block(input)
 
 
 class Bottleneck(nn.Module):
@@ -85,7 +16,12 @@ class Bottleneck(nn.Module):
     Identity Mapping in Deep Residual Networks
     ref https://arxiv.org/abs/1603.05027
     """
-    def __init__(self, inplanes, planes, stride=1, downsample=None, norm_layer=nn.BatchNorm2d):
+    def __init__(self,
+                 inplanes: int,
+                 planes: int,
+                 stride: int = 1,
+                 downsample: int = None,
+                 norm_layer: type = nn.BatchNorm2d) -> None:
         super(Bottleneck, self).__init__()
         self.expansion = 4
         self.downsample = downsample
@@ -103,7 +39,7 @@ class Bottleneck(nn.Module):
                        nn.Conv2d(planes, planes * self.expansion, kernel_size=1, stride=1)]
         self.conv_block = nn.Sequential(*conv_block)
         
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         if self.downsample is not None:
             residual = self.residual_layer(x)
         else:
@@ -116,7 +52,11 @@ class UpBottleneck(nn.Module):
     Enables passing identity all the way through the generator
     ref https://arxiv.org/abs/1703.06953
     """
-    def __init__(self, inplanes, planes, stride=2, norm_layer=nn.BatchNorm2d):
+    def __init__(self,
+                 inplanes: int,
+                 planes: int,
+                 stride: int = 2,
+                 norm_layer: type = nn.BatchNorm2d) -> None:
         super(UpBottleneck, self).__init__()
         self.expansion = 4
         self.residual_layer = UpsampleConvLayer(inplanes, planes * self.expansion,
@@ -132,18 +72,22 @@ class UpBottleneck(nn.Module):
                        nn.Conv2d(planes, planes * self.expansion, kernel_size=1, stride=1)]
         self.conv_block = nn.Sequential(*conv_block)
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         return  self.residual_layer(x) + self.conv_block(x)
 
 
 class ConvLayer(torch.nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride):
+    def __init__(self,
+                 in_channels: int,
+                 out_channels: int,
+                 kernel_size: int,
+                 stride: int) -> None:
         super(ConvLayer, self).__init__()
         reflection_padding = int(np.floor(kernel_size / 2))
         self.reflection_pad = nn.ReflectionPad2d(reflection_padding)
         self.conv2d = nn.Conv2d(in_channels, out_channels, kernel_size, stride)
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         out = self.reflection_pad(x)
         return self.conv2d(out)
 
@@ -154,7 +98,11 @@ class UpsampleConvLayer(torch.nn.Module):
     ref: http://distill.pub/2016/deconv-checkerboard/
     """
 
-    def __init__(self, in_channels, out_channels, kernel_size, stride, upsample=None):
+    def __init__(self, in_channels: int,
+                 out_channels: int,
+                 kernel_size: int,
+                 stride: int,
+                 upsample: float = None):
         super(UpsampleConvLayer, self).__init__()
         self.upsample = upsample
         if upsample:
@@ -177,7 +125,7 @@ class Inspiration(nn.Module):
     tuning the featuremap with target Gram Matrix
     ref https://arxiv.org/abs/1703.06953
     """
-    def __init__(self, C, B=1):
+    def __init__(self, C: int, B: int = 1) -> None:
         super(Inspiration, self).__init__()
         # B is equal to 1 or input mini_batch
         self.weight = nn.Parameter(torch.Tensor(1,C,C), requires_grad=True)
@@ -187,70 +135,29 @@ class Inspiration(nn.Module):
         self.C = C
         self.reset_parameters()
 
-    def reset_parameters(self):
+    def reset_parameters(self) -> None:
         self.weight.data.uniform_(0.0, 0.02)
 
-    def setTarget(self, target):
+    def setTarget(self, target: torch.Tensor) -> None:
         self.G = target
 
-    def forward(self, X):
+    def forward(self, X: torch.Tensor) -> torch.Tensor:
         # input X is a 3D feature map
         self.P = torch.bmm(self.weight.expand_as(self.G),self.G)
         return torch.bmm(self.P.transpose(1,2).expand(X.size(0), self.C, self.C), X.view(X.size(0),X.size(1),-1)).view_as(X)
 
-    def __repr__(self):
+    def __repr__(self) -> None:
         return self.__class__.__name__ + '(' \
             + 'N x ' + str(self.C) + ')'
 
 
-class Vgg16(torch.nn.Module):
-    def __init__(self):
-        super(Vgg16, self).__init__()
-        self.conv1_1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1)
-        self.conv1_2 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
-
-        self.conv2_1 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
-        self.conv2_2 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
-
-        self.conv3_1 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
-        self.conv3_2 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
-        self.conv3_3 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
-
-        self.conv4_1 = nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1)
-        self.conv4_2 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
-        self.conv4_3 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
-
-        self.conv5_1 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
-        self.conv5_2 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
-        self.conv5_3 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
-
-    def forward(self, X):
-        h = F.relu(self.conv1_1(X))
-        h = F.relu(self.conv1_2(h))
-        relu1_2 = h
-        h = F.max_pool2d(h, kernel_size=2, stride=2)
-
-        h = F.relu(self.conv2_1(h))
-        h = F.relu(self.conv2_2(h))
-        relu2_2 = h
-        h = F.max_pool2d(h, kernel_size=2, stride=2)
-
-        h = F.relu(self.conv3_1(h))
-        h = F.relu(self.conv3_2(h))
-        h = F.relu(self.conv3_3(h))
-        relu3_3 = h
-        h = F.max_pool2d(h, kernel_size=2, stride=2)
-
-        h = F.relu(self.conv4_1(h))
-        h = F.relu(self.conv4_2(h))
-        h = F.relu(self.conv4_3(h))
-        relu4_3 = h
-
-        return [relu1_2, relu2_2, relu3_3, relu4_3]
-
-
 class Net(nn.Module):
-    def __init__(self, input_nc=3, output_nc=3, ngf=64, norm_layer=nn.InstanceNorm2d, n_blocks=6, gpu_ids=[]):
+    def __init__(self,
+                 input_nc: int = 3,
+                 output_nc: int = 3,
+                 ngf: int = 64, norm_layer: type = nn.InstanceNorm2d,
+                 n_blocks: int = 6,
+                 gpu_ids: list = []) -> None:
         super(Net, self).__init__()
         self.gpu_ids = gpu_ids
         self.gram = GramMatrix()
@@ -283,12 +190,12 @@ class Net(nn.Module):
 
         self.model = nn.Sequential(*model)
 
-    def setTarget(self, Xs):
+    def setTarget(self, Xs: torch.Tensor) -> None:
         F = self.model1(Xs)
         G = self.gram(F)
         self.ins.setTarget(G)
 
-    def forward(self, input):
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
         return self.model(input)
 
 
--- core/style_transfer/photo_demo.py
+++ core/style_transfer/photo_demo.py
@@ -1,45 +1,44 @@
-from configparser import Interpolation
 import cv2
 import torch
-import os
 import pygame
 import pygame.camera
 
 from .net import Net
 from .utils import StyleLoader, predict_image
 
-def predict(args):
-    style_model = Net(ngf=args["ngf"])
-    model_dict = torch.load(args["model"])
+def predict(Config: dict) -> None:
+    style_model = Net(ngf=Config["ngf"])
+    model_dict = torch.load(Config["model"])
     model_dict_clone = model_dict.copy()
     for key, _ in model_dict_clone.items():
         if key.endswith(('running_mean', 'running_var')):
             del model_dict[key]
     style_model.load_state_dict(model_dict, False)
     style_model.eval()
-    style_loader = StyleLoader(args["style_folder"], args["style_size"])
+    style_loader = StyleLoader(Config["style_folder"], Config["style_size"])
 
-    content_image = cv2.imread(args["content_image"])
+    content_image = cv2.imread(Config["content_image"])
     pred_img = predict_image(content_image,
                                 style_loader,
                                 style_model,
-                                args["mirror"],
-                                args["style_idx"])  
-    if ("resize" in args):
-        if ("new_height" in args and "new_width" in args):
-            pred_img = cv2.resize(pred_img, (args["new_width"], args["new_height"]), interpolation = cv2.INTER_AREA)
-    cv2.imwrite(args["output_image"], pred_img)
+                                Config["mirror"],
+                                Config["style_idx"])  
+    if ("resize" in Config):
+        if ("new_height" in Config and "new_width" in Config):
+            pred_img = cv2.resize(pred_img, (Config["new_width"], Config["new_height"]), interpolation = cv2.INTER_AREA)
+    cv2.imwrite(Config["output_image"], pred_img)
     cv2.destroyAllWindows()
 
 
-def make_photo(height: int,
-               save_path: str) -> None:
-
+def make_photo(save_path: str) -> bool:
     pygame.camera.init()
-    pygame.camera.list_cameras() #Camera detected or not
-    cam = pygame.camera.Camera("/dev/video0",(800,600))
-    cam.start()
-    img = cam.get_image()
-    pygame.image.save(img, save_path)
-    cam.stop()
-    
+    cameras = pygame.camera.list_cameras() #Camera detected or not
+    if cameras:
+        cam = pygame.camera.Camera(cameras[0],(800,600)) #/dev/video0
+        cam.start()
+        img = cam.get_image()
+        pygame.image.save(img, save_path)
+        cam.stop()
+        return True
+    else:
+        return False

0a84017ca97219e860cdc5a66d5d673ed9b81344
Piotrekszmel <piotr.szmelczynski@gmail.com>
2022-02-11 20:57:15 +0100

    Create Config dict for program inputs

--- main.py
+++ main.py
@@ -1,65 +1,64 @@
-import os
 from flask import Flask, request, redirect, url_for, send_from_directory, render_template
+import os
 
 from core.style_transfer.photo_demo import predict, make_photo
-import cv2
 
 UPLOAD_FOLDER = "./"
 
-def allowed_file(filename):
+
+def allowed_file(filename: str):
     return "." in filename and \
            filename.rsplit(".", 1)[1].lower() in ["jpg", "jpeg"]
 
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
+Config = {
+    "model": "core/style_transfer/models/21styles.model",
+    "style_folder": "static/images/styles",
+    "style_size": 512,
+    "demo_size": 480,
+    "output_image": "output.jpg",
+    "mirror": False,
+    "if_download": 0,
+    "style_idx": 0,
+    "ngf": 128,
+    "content_image": "content.jpg",
+    "save_path": os.path.join(app.config["UPLOAD_FOLDER"], "content.jpg")
+}
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    args = {
-        "ngf": 128,
-        "style_idx": 0,
-        "model": "core/style_transfer/models/21styles.model",
-        "style_folder": "static/images/styles",
-        "style_size": 512,
-        "demo_size": 480,
-        "output_image": "output.jpg",
-        "mirror": False,
-        "if_download": 0,
-        "style_idx": 0,
-        "content_image": "content.jpg",
-        "save_path": os.path.join(app.config["UPLOAD_FOLDER"], "content.jpg"),
-    }
-     
     if request.method == "POST":
         if request.form.get("checkpoint"):
-            args["style_idx"] = request.form["checkpoint"]
+            Config["style_idx"] = int(request.form["checkpoint"])
         if request.form.get("mirror"):
-            args["mirror"] = True
+            Config["mirror"] = True
         if request.form.get("if_download"):
-            args["if_download"] = 1
+            Config["if_download"] = 1
         if request.form.get("resize"):
-            args["resize"] = True
+            Config["resize"] = True
             if request.form.get("new_height"):
-                args["new_height"] = int(request.form.get("new_height"))
+                Config["new_height"] = int(request.form.get("new_height"))
             if request.form.get("new_width"):
-                args["new_width"] = int(request.form.get("new_width"))   
+                Config["new_width"] = int(request.form.get("new_width"))   
         if "file" in request.files:
             file = request.files["file"]
             if file.filename != "" and allowed_file(file.filename):
-                file.save(args["save_path"])
-                predict(args)
-                return redirect(url_for("output", filename=args["output_image"], if_download=args["if_download"]))
+                file.save(Config["save_path"])
+                predict(Config)
+                return redirect(url_for("output", filename=Config["output_image"], if_download=Config["if_download"]))
             else:
-                make_photo(int(args["demo_size"]), args["save_path"])
-                predict(args)
-                return redirect(url_for("output", filename=args["output_image"], if_download=args["if_download"]))
+                result = make_photo(Config["save_path"])
+                if result:
+                    predict(Config)
+                    return redirect(url_for("output", filename=Config["output_image"], if_download=Config["if_download"]))
         return redirect(request.url)
     
     return render_template("index.html")
 
 @app.route("/output/<filename>/<if_download>", methods=["GET"])
-def output(filename, if_download):
+def output(filename: str, if_download: str):
     return send_from_directory(app.config["UPLOAD_FOLDER"],
                                filename, as_attachment=int(if_download))
 

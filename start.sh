1371da17198654be96593c9ef3f87e2033377a0a
Piotrekszmel <piotr.szmelczynski@gmail.com>
2022-02-15 20:22:05 +0100

    add downloading the model to the start.sh script

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+if [ ! -e  deepimage/core/style_transfer/models]; then
+    mkdir deepimage/core/style_transfer/models
+    wget -O deepimage/core/style_transfer/models/21styles.model https://www.dropbox.com/s/2iz8orqqubrfrpo/21styles.model?dl=1
+fi
 app="deep-image"
 docker build -t ${app} .
 docker run -d -p 5000:5000 \
